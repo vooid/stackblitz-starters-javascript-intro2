ba171f8d478a6877fdc4a0320b80e756e5e0a01d
vooid <eirikm@gmail.com>
2024-05-15 20:04:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-javascript-intro2
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/vooid/stackblitz-starters-javascript-intro2)
---
+++ script.js
@@ -0,0 +1,35 @@
+console.log('Hello!');
+console.log("Ta deg ei bolle!");
+
+// Datatyper som vi kan lagre i variabler
+
+const myBool = true; 
+const myNumber = 666; 
+const myStr = "the number of the BEAST!";
+const myExpl = " is the ";
+const myUndEfined = undefined;
+const myVoid = null;
+
+let myChaos = Math.random();
+
+const myListOfNumberOfTheBeast = [616, 666];
+
+
+if(myChaos < 0,5){
+  console.log(myListOfNumberOfTheBeast[0]+myExpl+myStr);
+}
+else{
+  console.log(myListOfNumberOfTheBeast[1]+myExpl+myStr);
+}
+console.log(myListOfNumberOfTheBeast);
+console.log(myListOfNumberOfTheBeast[1]);
+console.log(myChaos);
+
+//objekter
+const laura = {
+  navn: "Laura",
+  alder: "27",
+  høgd: 171, 
+}
+console.log(laura);
+console.log(laura.alder);
