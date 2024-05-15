79a00602cb2b5967e8d44eb965d554f39b2b0229
vooid <eirikm@gmail.com>
2024-05-15 20:57:01 +0200

    22

--- script.js
+++ script.js
@@ -11,14 +11,13 @@ const myUndEfined = undefined;
 const myVoid = null;
 
 let myChaos = Math.random();
-
+let chaosMe = myChaos < 0.5;
 const myListOfNumberOfTheBeast = [616, 666];
 
 
-if(myChaos < 0,5){
+if(chaosMe) {
   console.log(myListOfNumberOfTheBeast[0]+myExpl+myStr);
-}
-else{
+} else{ 
   console.log(myListOfNumberOfTheBeast[1]+myExpl+myStr);
 }
 console.log(myListOfNumberOfTheBeast);
@@ -38,3 +37,45 @@ console.log(laura.navn);
 let nummerA = 10;
 let nummerB = 32;
 console.log(nummerA+nummerB);
+
+const erStor = 1024;
+const erStørre = 8094;
+const erEndaStørre = erStor < erStørre;
+console.log(erEndaStørre);
+
+const hhh = erStor > erStørre;
+console.log(hhh);
+// løkker
+let teljar = 0;
+while(teljar <= 19){
+  console.log(teljar)
+  teljar = teljar +1
+}
+console.log("---")
+do{
+  console.log(teljar)
+  teljar = teljar + 1
+} while (teljar < 23)
+console.log("---")
+for (  ; teljar < 28 ; teljar++){
+  console.log(teljar)
+}
+
+//const rot = [ "spade", " maske", "dildo", "bøtte", "lingerine"]
+//rot.sort
+//for (let a = 0; a = rot.length; a++){
+//  console.log(rot[a])
+//}
+//console.log(rot.length)
+
+function heisann(navn) {
+  console.log("Halla "+ navn)
+}
+
+heisann(laura.navn)
+
+function dobbel(nummer){
+  return nummer*2;
+}
+
+console.log("Laura er " + dobbel(laura.alder))
